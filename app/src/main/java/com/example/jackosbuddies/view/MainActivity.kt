acfd806b7e8615ad648daf70b70e723486dc25fa
nouraloudani <contact.nouraloudani@gmail.comgit config --global user.name nlouda95>
2021-09-02 11:34:38 -0400

    MVVM folders

--- app/src/main/java/com/example/jackosbuddies/view/MainActivity.kt
+++ app/src/main/java/com/example/jackosbuddies/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.jackosbuddies
+package com.example.jackosbuddies.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.jackosbuddies.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
