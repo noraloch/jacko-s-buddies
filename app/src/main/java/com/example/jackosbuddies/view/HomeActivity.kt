676c67aa5661814273dd0a292c89b25e8c3a96bf
nouraloudani <contact.nouraloudani@gmail.comgit config --global user.name nlouda95>
2021-09-09 02:41:22 -0400

    finished up to 0909

--- app/src/main/java/com/example/jackosbuddies/view/HomeActivity.kt
+++ app/src/main/java/com/example/jackosbuddies/view/HomeActivity.kt
@@ -3,17 +3,23 @@ package com.example.jackosbuddies.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.jackosbuddies.R
 import com.example.jackosbuddies.databinding.ActivityHomeBinding
 import com.example.jackosbuddies.viewmodel.KatViewModel
-
 
 class HomeActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityHomeBinding.inflate(layoutInflater) }
     private val katViewModel by viewModels<KatViewModel>()
+    private val navHostFragment by lazy {
+        supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        binding.bottomNavigation.setupWithNavController(navHostFragment.navController)
     }
 }
